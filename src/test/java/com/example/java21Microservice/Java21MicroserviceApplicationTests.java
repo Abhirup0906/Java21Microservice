304e3d5bd2e9ac47a27f9647a1945a23bd049480
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2023-12-24 14:57:54 +0530

    basic project added

---
+++ src/test/java/com/example/java21Microservice/Java21MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java21Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
