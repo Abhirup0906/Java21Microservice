f961511606e702026fab427d50e48737dd5b75f8
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2024-01-06 13:33:06 +0530

    Criteria API setup with meta model

--- src/main/java/com/example/java21Microservice/Controllers/EmployeeApi.java
+++ src/main/java/com/example/java21Microservice/Controllers/EmployeeApi.java
@@ -1,14 +1,28 @@
 package com.example.java21Microservice.Controllers;
 
+import com.example.java21Microservice.DbEntities.Employee;
+import com.example.java21Microservice.Repositories.Query.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeApi {
+
+    @Autowired
+    EmployeeRepository repository;
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    @GetMapping("/getDevelopmentEngineer")
+    public List<Employee> getDevelopmentEngineer() {
+
+        return repository.getDevelopmentEngineer();
     }
 }
--- src/main/java/com/example/java21Microservice/Repositories/Query/EmployeeRepository.java
+++ src/main/java/com/example/java21Microservice/Repositories/Query/EmployeeRepository.java
@@ -1,15 +1,35 @@
 package com.example.java21Microservice.Repositories.Query;
 
 import com.example.java21Microservice.DbEntities.Employee;
-import com.example.java21Microservice.ResponseModel.EmployeeDepartment;
+import com.example.java21Microservice.DbEntities.Employee_;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Repository
+@Repository
 public class EmployeeRepository {
 
-    public List<EmployeeDepartment> getEmployeeDepartmentDetails()
+    @Autowired
+    EntityManager entityManager;
+
+    public List<Employee> getDevelopmentEngineer()
     {
-        return new ArrayList<EmployeeDepartment>();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Employee> empQuery = builder.createQuery(Employee.class);
+        Root<Employee> root = empQuery.from(Employee.class);
+        //ParameterExpression<Long> value = builder.parameter(Long.class);
+        empQuery.select(root).where(builder.equal(root.get(Employee_.JOB_TITLE), "Research and Development Engineer"));
+
+        //TypedQuery<Contact> query = em.createQuery(contactQuery);
+        //query.setParameter(value, id);
+        return entityManager.createQuery(empQuery).getResultList();
     }
 }
